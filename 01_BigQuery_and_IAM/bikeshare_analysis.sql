4e048f444846d7b6975ea4121b043e4aff79e1b7
Prats0404 <prathvi.22ad038@sode-edu.in>
2026-03-20 12:37:02 +0530

    Add query to calculate average duration by subscriber type

--- 01_BigQuery_and_IAM/bikeshare_analysis.sql
+++ 01_BigQuery_and_IAM/bikeshare_analysis.sql
@@ -11,3 +11,7 @@ FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 GROUP BY start_station_name
 ORDER BY total_trips DESC
 LIMIT 5;
+
+SELECT subscriber_type, AVG(duration_minutes) AS average_duration
+FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
+GROUP BY subscriber_type;
