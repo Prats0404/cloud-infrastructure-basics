fa580cd71f8882e681591d8c05bd3eac5231bec8
Prats0404 <prathvi.22ad038@sode-edu.in>
2026-03-20 12:31:39 +0530

    Add query for counting long bikeshare trips
    Added a query to count total long trips over 180 minutes.

--- 01_BigQuery_and_IAM/bikeshare_analysis.sql
+++ 01_BigQuery_and_IAM/bikeshare_analysis.sql
@@ -1,3 +1,7 @@
 # task-1:
 SELECT * FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 LIMIT 10;
+
+SELECT COUNT(*) AS total_long_trips
+FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
+WHERE duration_minutes > 180;
