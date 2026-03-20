ea0d28aaa2f3652518ec94f3223c8a7b1926fa50
Prats0404 <prathvi.22ad038@sode-edu.in>
2026-03-20 12:34:04 +0530

    Add query to count trips by start station

--- 01_BigQuery_and_IAM/bikeshare_analysis.sql
+++ 01_BigQuery_and_IAM/bikeshare_analysis.sql
@@ -5,3 +5,9 @@ LIMIT 10;
 SELECT COUNT(*) AS total_long_trips
 FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 WHERE duration_minutes > 180;
+
+SELECT start_station_name, COUNT(*) AS total_trips
+FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
+GROUP BY start_station_name
+ORDER BY total_trips DESC
+LIMIT 5;
