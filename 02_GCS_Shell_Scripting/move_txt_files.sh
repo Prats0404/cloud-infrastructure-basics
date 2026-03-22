5af74e674a71f6e338df8f32a80ab8af80fab002
Prats0404 <prathvi.22ad038@sode-edu.in>
2026-03-22 16:37:33 +0530

    Create move_txt_files.sh for GCS file transfer
    Added a script to move .txt files between GCS buckets.

--- 02_GCS_Shell_Scripting/move_txt_files.sh
+++ 02_GCS_Shell_Scripting/move_txt_files.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# Define the source and destination buckets
+SOURCE_BUCKET="gs://your-source-bucket-name"
+DEST_BUCKET="gs://your-destination-bucket-name"
+
+# Step 1: Log that the process is starting
+echo "Starting transfer of .txt files from $SOURCE_BUCKET to $DEST_BUCKET..."
