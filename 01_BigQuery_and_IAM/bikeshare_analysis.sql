008dc194b6608284dae48aec9ccf74dcc41a4282
Prats0404 <prathvi.22ad038@sode-edu.in>
2026-03-22 16:35:17 +0530

    Revise task comments and add bonus query
    Updated task comments and added a bonus query to analyze trips by year.

--- 01_BigQuery_and_IAM/bikeshare_analysis.sql
+++ 01_BigQuery_and_IAM/bikeshare_analysis.sql
@@ -1,17 +1,23 @@
-# task-1:
+#task-1:
 SELECT * FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 LIMIT 10;
-# task-2:
+#task-2:
 SELECT COUNT(*) AS total_long_trips
 FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 WHERE duration_minutes > 180;
-# task-3:
+#task-3:
 SELECT start_station_name, COUNT(*) AS total_trips
 FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 GROUP BY start_station_name
 ORDER BY total_trips DESC
 LIMIT 5;
-# task-4:
+#task-4:
 SELECT subscriber_type, AVG(duration_minutes) AS average_duration
 FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
 GROUP BY subscriber_type;
+#Bonus
+SELECT EXTRACT(YEAR FROM start_time) AS trip_year, COUNT(*) AS total_trips
+FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips`
+GROUP BY trip_year
+ORDER BY total_trips DESC
+LIMIT 1;
